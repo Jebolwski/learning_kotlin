bbe58a7106b36f091f0c74d1fcb01ffb4e82eb48
Mert Gökmen <mertgkmeen@gmail.com>
2024-01-18 18:05:14 +0300

    trying google login

--- app/src/main/java/com/example/greetingcard/MainActivity.kt
+++ app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -4,8 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,5 +19,9 @@ class MainActivity : AppCompatActivity() {
             intent = Intent(this,CalculatorActivity::class.java)
             startActivity(intent)
         }
+
+
     }
+
+
 }
